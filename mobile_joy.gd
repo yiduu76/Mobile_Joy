b980ba246cfd055bfb9247cce1635c6568c89181
yiduu76 <1023104732@qq.com>
2024-02-13 18:20:58 +0800

    first commit

---
+++ mobile_joy.gd
@@ -0,0 +1,19 @@
+extends Control
+@onready var is_press=false
+@onready var out_pos=Vector2.ZERO
+@onready var out_joy=$out_joy
+@onready var in_joy=$in_joy
+
+func _input(event):
+	if event is InputEventScreenTouch:
+		if event.pressed==true:
+			is_press=true
+			out_pos=get_local_mouse_position()
+		elif event.pressed==false:
+			is_press=false
+func _process(_delta):
+	if is_press:
+		out_joy.position=out_pos
+		in_joy.position=out_joy.position-50*(out_joy.position-get_local_mouse_position()).normalized()
+	out_joy.visible=is_press
+	in_joy.visible=is_press
